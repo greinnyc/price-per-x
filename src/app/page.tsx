ee4a5b53fe0ce6311187aa478fa02e2d0dc56d22
Greinny <greinny@gmail.com>
2025-03-19 00:44:02 -0500

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import React, { useState } from 'react';
+import { Button, Form, Input, Radio } from 'antd';
+
+type LayoutType = Parameters<typeof Form>[0]['layout'];
+
+export default function Home() {
+  const [form] = Form.useForm();
+
+  return (
+    <Form
+      layout={'inline'}
+      form={form}
+      style={{ maxWidth: 'none' }}
+    >
+      <Form.Item label="Cantidad">
+        <Input placeholder="Ingresar cantidad" />
+      </Form.Item>
+      <Form.Item label="Precio">
+        <Input placeholder="Ingresar precio" />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary">Calcular</Button>
+      </Form.Item>
+    </Form>
+  );
+}
